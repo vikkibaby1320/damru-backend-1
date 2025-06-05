ea6a0474df39f1de95880929949dee7ee37c7a9e
vikkibaby1320 <vikkibaby1320@gmail.com>
2025-06-05 14:31:33 +0530

    Added Admin model file

---
+++ models/Admin.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+adminSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;
